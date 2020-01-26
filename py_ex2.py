cc47c129849ffa3644a7e6bbd3d6728369bec5a8
SupraBlatt <yuvalezra2001@gmail.com>
2020-01-26 18:16:12 +0200

    ima shelha uwu

--- py_ex2.py
+++ py_ex2.py
@@ -9,7 +9,6 @@ def main():
                   factory.get_algorithm("bayes")]
 
     training = DataSet()
-    print("gay" + training[0])
     target = training.load_from_file("train.txt")
 
     validation = DataSet()
@@ -17,14 +16,11 @@ def main():
 
     output_file = "output.txt"
     another_one = DataSet()
-    print("k = ", len(another_one))
 
     # run the algorithms
     acc = []
     for i in range(len(algorithms)):
-        print("Starting alg ", i)
         accuracy = tests.validate(algorithms[i], training, validation, target)
-        print("accuracy = ", accuracy)
         # print specifically the tree algorithm
         if i == 0:
             algorithms[i].print_tree(output_file)
@@ -32,7 +28,6 @@ def main():
 
     # write the accuracy into the files
     accuracy_string = '\n' + '\t'.join(acc)
-    print("Overall accuracy string =", accuracy_string)
     with open(output_file, 'a') as acc_file:
         acc_file.write(accuracy_string)
 
